f0b2eac50671d00b1418387a107444ba33620506
Jaimin <jaiminjaffer123@gmail.com>
2021-02-27 02:51:58 -0500

    adds registration data to database

---
+++ server.js
@@ -0,0 +1,136 @@
+if (process.env.NODE_ENV != 'production') {
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const { password } = require('./password.js')
+const url = password;
+
+
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+
+
+// MongoClient.connect(url, function (err, client) {
+//     assert.equal(null, err);
+
+
+//     const db = client.db("login");
+
+//     var cursor = db.collection('users').find({ email: user.email });
+    
+//     function iterateFunc(doc) {
+//         console.log(JSON.stringify(doc, null, 4));
+//     }
+    
+//     function errorFunc(error) {
+//         console.log(error);
+//     }
+    
+//     cursor.forEach(iterateFunc, errorFunc);
+    
+//     client.close();
+// });
+
+
+const users = []
+
+app.set('vie-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', async (req, res) => {
+
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+        MongoClient.connect(url, function (err, client) {
+            assert.equal(null, err);
+
+
+            const db = client.db("login");
+
+            db.collection('users').insertOne({
+                id: Date.now().toString(),
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword
+            })
+                .then(function (result) {
+                    // process result
+                })
+
+            client.close();
+        });
+
+        res.redirect('/login')
+    } catch (error) {
+        res.redirect('/register')
+    }
+})
+
+
+// app.post('/register', async (req, res) => {
+
+//     try {
+//         const hashedPassword = await bcrypt.hash(req.body.password, 10)
+//         users.push({
+//             id: Date.now().toString(),
+//             name: req.body.name,
+//             email: req.body.email,
+//             password: hashedPassword
+//         })
+//         res.redirect('/login')
+//     } catch (error) {
+//         res.redirect('/register')
+//     }
+//     console.log(users)
+// })
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/login')
+}
+
+app.listen(3000)
