c2f2868b80c3dd48f8d15871d175a10d1b2d0263
Jaimin <jaiminjaffer123@gmail.com>
2021-02-27 02:55:21 -0500

    minor adjustments

--- passport-config.js
+++ passport-config.js
@@ -5,7 +5,7 @@ function initialize(passport, getUserbyEmail, getUserbyId) {
     const authenticateUser = async (email, password, done) => {
         const user = getUserbyEmail(email)
         if (user == null) {
-            return done(null, false, { message: 'No user with that email' })
+            return done(null, false, { message: "User doesn't exist" })
         }
 
         try {
