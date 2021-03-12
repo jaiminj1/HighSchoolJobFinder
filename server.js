0fdf854ff198043808a5ef0d14f85762feb1aaf0
Jaimin <jaiminjaffer123@gmail.com>
2021-03-12 00:15:38 -0500

    update server.js

--- server.js
+++ server.js
@@ -373,24 +373,21 @@ function isLoggedIn(req, res, next) {
 
 //current example code taken from https://nodemailer.com/about/ 
 async function sendEmail(email, code) {
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    let testAccount = await nodemailer.createTestAccount();
 
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // true for 465, false for other ports
         auth: {
-            user: testAccount.user, // generated ethereal user
-            pass: testAccount.pass, // generated ethereal password
+            user: process.env.EMAIL, // generated ethereal user
+            pass: process.env.PASSWORD, // generated ethereal password
         },
     });
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
-        from: '"High School JobFinder" <no-reply@jobfinder.com>', // sender address
+        from: '"Testing Account" <no-reply@jobfinder.com>', // sender address
         to: email, // list of receivers
         subject: "Confirmation Code", // Subject line
         text: "Your confirmation code is " + code, // plain text body
@@ -398,11 +395,6 @@ async function sendEmail(email, code) {
     });
 
     console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 //sendEmail().catch(console.error);
 
