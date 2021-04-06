db8b3bf6619774df5e028b1f58750b61dfd5832a
Dhirhan Kanesalingam <dhirhan17@gmail.com>
2021-04-05 22:22:19 -0400

    Small updates

--- server.js
+++ server.js
@@ -962,7 +962,7 @@ app.put("/admin-portal/admin-userview", isLoggedIn, isAdmin, function (req, res)
 
 //admin job view page
 app.get("/admin-portal/admin-jobview", isLoggedIn, isAdmin, function (req, res) {
-    res.render("admin-portal/admin-jobview", { result: false, error: false });
+    res.render("admin-portal/admin-jobview", { result: false, error: false, discipline:"", type:"", term:"" });
 });
 
 
@@ -1007,7 +1007,7 @@ app.post("/admin-portal/admin-myaccount", isLoggedIn, isAdmin, function (req, re
 var jobpostCollection;
 
 app.get("/student-portal/student-findjobs", (req, res) => {
-    res.render("student-portal/student-findjobs", { result: false });
+    res.render("student-portal/student-findjobs", { result: false, discipline:"", type:"", term:"" });
 })
 
 
@@ -1049,10 +1049,10 @@ app.get("/search", isLoggedIn, async function (req, res) {
             }
         ]).toArray();
         if (req.user.accountType == "student") {
-            res.render("student-portal/student-findjobs", { result: result })
+            res.render("student-portal/student-findjobs", { result: result, term: query, type: type, discipline: discipline })
         }
         else if (req.user.accountType == "admin") {
-            res.render("admin-portal/admin-jobview", { result: result })
+            res.render("admin-portal/admin-jobview", { result: result, term: query, type: type, discipline: discipline })
         }
 
     } catch (e) {
